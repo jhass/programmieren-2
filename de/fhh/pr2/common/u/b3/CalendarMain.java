573354d7c99025f20f4b09ebb92b15e6af0c3eaa
Jonne Haß <me@mrzyx.de>
2012-06-19 23:13:15 +0200

    copy over b4, commit what was left uncommitted

---
+++ de/fhh/pr2/common/u/b3/CalendarMain.java
@@ -0,0 +1,21 @@
+package de.fhh.pr2.common.u.b3;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class CalendarMain {
+	public static void main(String[] args) {
+		Calendar localCal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"),
+												 new Locale("de", "DE"));
+		Calendar budCal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Bangkok"),
+											   new Locale("th", "TH"));
+		localCal.set(2009, 11, 24, 10, 30, 0);
+		budCal.setTime(localCal.getTime());
+		DateFormat df = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.LONG, new Locale("de", "DE"));
+		df.setCalendar(budCal);
+		System.out.println(df.format(budCal.getTime()));
+	}
+}
+	
---
+++ de/fhh/pr2/common/u/b3/Strange.java
@@ -0,0 +1,36 @@
+package de.fhh.pr2.common.u.b3;
+
+import java.awt.print.*;
+import java.net.*;
+import java.security.cert.*;
+import java.sql.*;
+import java.io.*;
+
+import javax.naming.*;
+import javax.sql.rowset.spi.*;
+
+public class Strange {
+	public static void main(String[] args) {
+		try {
+			f();
+		} catch (RuntimeException | // ArithmeticException, ArrayStoreException
+				 Error | // IOError, AssertionError
+				 CertificateException | // self, CertificateNotYetValidException
+				 NamingException | // self, InvalidNameException, OperationNotSupportedException
+				 IOException | // self, MalformedURLException, FileNotFoundException
+				 CloneNotSupportedException | // self 	
+				 PrinterException | // self
+				 SQLException e // self, SyncProviderException
+				) {
+			e.printStackTrace();
+		}
+	}
+	public static void f() throws ArithmeticException, NamingException,
+			InvalidNameException, IOException, MalformedURLException,
+			SyncProviderException, CertificateNotYetValidException, IOError,
+			OperationNotSupportedException, ArrayStoreException,
+			CloneNotSupportedException, FileNotFoundException,
+			PrinterException, SQLException, CertificateException,
+			AssertionError {
+	}
+}
